41896a74709245a6e1c32edbd8abe426b95896f6
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2015-04-11 14:53:35 -0300

    Att da ativida 2 ex2
    Agora a data ta validando

--- src/org/jairo/matos/Ex2Data.java
+++ src/org/jairo/matos/Ex2Data.java
@@ -4,66 +4,82 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Ex2Data {
-	private String  dia;
-	private String  mes;
-	private String ano;
-	
+	private int  dia;
+	private int  mes;
+	private int ano;
+	private boolean erro;
+	/**
+	 * Contrutor da Class
+	 */
+	public void Ex2Cal(){
+		erro=false;
+	}	
 	/**
 	 * Metodo de pergar a dia
 	 * @return dia retorna o dia
 	 */
-	public String getDia(){
+	public int getDia(){
 		return dia;
 	}
 	/**
 	 * Metodo de selecionar o dia
 	 * @param x recebe o dia para passar para variavel dia
 	 */
-	public void setDia(String x){
-		dia=x;
+	public void setDia(int x){
+		if(x>=1&&x<=31){dia=x;}
+		else{erro=true;}
+		
 	}
 	/**
 	 * Metodo de pergar a mes
 	 * @return mes retorna o mes
 	 */
-	public String getMes(){
+	public int getMes(){
 		return mes;
 	}
 	/**
 	 * Metodo de selecionar o mes
 	 * @param x recebe o dia para passar para variavel mes
 	 */
-	public void setMes(String x){
-		mes=x;
+	public void setMes(int x){
+		if( x>=1 && x<=12){mes=x;}
+		else{erro=true;}
+		
 	}
 	/**
 	 * Metodo de pergar a ano
 	 * @return ano retorna o ano
 	 */
-	public String getAno(){
+	public int getAno(){
 		return ano;
 	}
 	/**
 	 * Metodo de selecionar o ano
 	 * @param x recebe o dia para passar para variavel ano
 	 */
-	public void setAno(String x){
-		ano=x;
+	public void setAno(int x){
+		if(x>0){ano=x;}
+		else{erro=true;}
 	}
 	/**
 	 * Metodo de mostra a data  completa
 	 */
 	public void dataCompleta(){
-		String dataComp = dia+"/"+mes+"/"+ano;//Garda a data completa
-		Date d = new Date();
+	
+		String dataComp = ""+dia+"/"+mes+"/"+ano+"";//Garda a data completa
+		Date dt = new Date();
 		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");//Escolar o formato da data
 		//Checa se a data é valida
 		try {
-			d = sdf.parse(dataComp);
+			dt = sdf.parse(dataComp);
 		} catch (ParseException e) {
 			
 			e.printStackTrace();
 		}
-		System.out.println(sdf.format(d));//emprima a data no monitor
+		if(erro){
+			System.out.println("Erro no sistema data informada invalida");
+		}else{
+			System.out.println(sdf.format(dt));//emprima a data no monitor
+		}
 	}
 }
--- src/org/jairo/matos/TestEx2Data.java
+++ src/org/jairo/matos/TestEx2Data.java
@@ -4,9 +4,9 @@ public class TestEx2Data {
 
 	public static void main(String[] args) {
 	Ex2Data d =new Ex2Data();
-	d.setAno("2000");
-	d.setDia("100");
-	d.setMes("05");
+	d.setAno(2000);
+	d.setDia(12);
+	d.setMes(05);
 	d.dataCompleta();
 	System.out.println(Ex3Cal.soma(2,2));
 	System.out.println(Ex3Cal.mult(2,2));
